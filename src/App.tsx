83feca16c210d8b6106ca927f608b6bbd600bbd6
Raul <you@example.com>
2022-09-07 09:55:45 +0200

    set search bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <header>
+      <h1>Radio Factoría!</h1>
+    </header>
+    <section className='search-bar'>
+      <input type="text"
+             placeholder='Escribe el nombre de la radio' />
+      <button name="search">Buscar</button>
+    </section>
+    </>
   );
 }
 
