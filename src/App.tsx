f05fc162f4b658fac233ccd38b0a4108655fde17
Raul <you@example.com>
2022-09-07 10:31:32 +0200

    click event added to search button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import axios from 'axios';
+import { ChangeEvent, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [search,setSearch]=useState('');
+  const [list, setList]=useState([]);
+  const handleChange = (e: ChangeEvent<HTMLInputElement> /* Forma elegante de tipar el elemento */)=>{
+    // forma cutre de tipar el elemento input
+    // setSearch((e.target as HTMLInputElement).value);
+    setSearch(e.target.value);
+  };
+  const getStations = ()=>{
+    const url = `http://95.179.139.106/json/stations/search?name=${search}`;
+    axios.get(url)
+      .then(result=> {
+        console.log(result.data);
+        setList(result.data)} )
+      .catch(error=>console.error(error))
+    // console.log('hola')
+  }
   return (
     <>
     <header>
@@ -8,8 +26,12 @@ function App() {
     </header>
     <section className='search-bar'>
       <input type="text"
-             placeholder='Escribe el nombre de la radio' />
-      <button name="search">Buscar</button>
+             placeholder='Escribe el nombre de la radio'
+             value={search}
+             onChange={handleChange} />
+      <button name="search"
+              onClick={getStations}>Buscar</button>
+      {search}
     </section>
     </>
   );
